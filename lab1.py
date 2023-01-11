b1b05a4b33a4047efe0feaffdcaaff020f712078
rydengraham <ryden3.14@gmail.com>
2023-01-11 12:23:33 -0700

    Made script print itself from github.

--- lab1.py
+++ lab1.py
@@ -3,3 +3,6 @@ import requests
 print(requests.__version__)
 
 print(requests.get("http://www.google.com/"))
+
+
+print(requests.get("https://raw.githubusercontent.com/rydengraham/cmput404lab1/master/lab1.py").content)
