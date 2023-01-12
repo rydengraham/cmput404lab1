a2137909db05c43797351da88f7b40e110ed8fd8
rydengraham <ryden3.14@gmail.com>
2023-01-11 17:27:56 -0700

    Swapped from .content to .text to match lab demo.

--- lab1.py
+++ lab1.py
@@ -2,7 +2,6 @@ import requests
 
 print(requests.__version__)
 
-print(requests.get("http://www.google.com/"))
+print(requests.get("http://www.google.com/").text)
 
-
-print(requests.get("https://raw.githubusercontent.com/rydengraham/cmput404lab1/master/lab1.py").content)
+print(requests.get("https://raw.githubusercontent.com/rydengraham/cmput404lab1/master/lab1.py").text)
